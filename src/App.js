1dcd3325b7cbfa50f2336e18e9cd5d77bf11d1c2
Renee O. <reneeosbourne@gmail.com>
2024-03-23 20:43:17 -0400

    Updating data model and landing page

--- src/App.js
+++ src/App.js
@@ -13,128 +13,121 @@ import {
   View,
   withAuthenticator,
 } from "@aws-amplify/ui-react";
-import { listNotes } from "./graphql/queries";
-import {
-  createNote as createNoteMutation,
-  deleteNote as deleteNoteMutation,
-} from "./graphql/mutations";
 import { generateClient } from 'aws-amplify/api';
-import { uploadData, getUrl, remove } from 'aws-amplify/storage';
+//import { uploadData, getUrl, remove } from 'aws-amplify/storage';
+import { generateClient } from "aws-amplify/api";
+import { updateTransactions } from './graphql/mutations';
+import { listTransactionss, getTransactions } from "./graphql/queries";
 
-const client = generateClient();
+const client = generateClient()
 
-const App = ({ signOut }) => {
-  const [notes, setNotes] = useState([]);
-
-  useEffect(() => {
-    fetchNotes();
-  }, []);
-
-  async function fetchNotes() {
-    const apiData = await client.graphql({ query: listNotes });
-    const notesFromAPI = apiData.data.listNotes.items;
-    await Promise.all(
-      notesFromAPI.map(async (note) => {
-        if (note.image) {
-          const url = await getUrl({ key: note.name });
-          note.image = url.url;
-        }
-        return note;
-      })
-    );
-    setNotes(notesFromAPI);
+const client = generateClient()
+const newTransactions = await client.graphql({
+  query: createTransactions,
+  variables: {
+      input: {
+  "name": "Lorem ipsum dolor sit amet",
+  "category": "Lorem ipsum dolor sit amet",
+  "amount": "Lorem ipsum dolor sit amet",
+  "transactionDate": "Lorem ipsum dolor sit amet",
+  "type": "Lorem ipsum dolor sit amet",
+  "description": "Lorem ipsum dolor sit amet"
+}
   }
+});
 
-  async function createNote(event) {
-    event.preventDefault();
-    const form = new FormData(event.target);
-    const image = form.get("image");
-    const data = {
-      name: form.get("name"),
-      description: form.get("description"),
-      image: image.name,
-    };
-    if (!!data.image) await uploadData({ key: data.name, data: image });
-    await client.graphql({
-      query: createNoteMutation,
-      variables: { input: data },
-    });
-    fetchNotes();
-    event.target.reset();
+const updatedTransactions = await client.graphql({
+  query: updateTransactions,
+  variables: {
+      input: {
+  "name": "Lorem ipsum dolor sit amet",
+  "category": "Lorem ipsum dolor sit amet",
+  "amount": "Lorem ipsum dolor sit amet",
+  "transactionDate": "Lorem ipsum dolor sit amet",
+  "type": "Lorem ipsum dolor sit amet",
+  "description": "Lorem ipsum dolor sit amet"
+}
   }
-  
-
-  async function deleteNote({ id, name }) {
-    const newNotes = notes.filter((note) => note.id !== id);
-    setNotes(newNotes);
-    await remove({ key: name });
-    await client.graphql({
-      query: deleteNoteMutation,
-      variables: { input: { id } },
-    });
+});
+const deletedTransactions = await client.graphql({
+  query: deleteTransactions,
+  variables: {
+      input: {
+          id: "YOUR_RECORD_ID"
+      }
   }
+});
 
-  return (
-    <View className="App">
-      <Heading level={1}>My Notes App</Heading>
-      <View as="form" margin="3rem 0" onSubmit={createNote}>
-        <Flex direction="row" justifyContent="center">
-          <TextField
-            name="name"
-            placeholder="Note Name"
-            label="Note Name"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <TextField
-            name="description"
-            placeholder="Note Description"
-            label="Note Description"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <Button type="submit" variation="primary">
-            Create Note
+// List all items
+const allTransactionss = await client.graphql({
+  query: listTransactionss
+});
+console.log(allTransactions);
+
+// Get a specific item
+const oneTransactions = await client.graphql({
+  query: getTransactions,
+  variables: { id: 'YOUR_RECORD_ID' }
+});
+return (
+  <View className="App">
+    <Heading level={1}>My Notes App</Heading>
+    <View as="form" margin="3rem 0" onSubmit={createNote}>
+      <Flex direction="row" justifyContent="center">
+        <TextField
+          name="name"
+          placeholder="Note Name"
+          label="Note Name"
+          labelHidden
+          variation="quiet"
+          required
+        />
+        <TextField
+          name="description"
+          placeholder="Note Description"
+          label="Note Description"
+          labelHidden
+          variation="quiet"
+          required
+        />
+        <Button type="submit" variation="primary">
+          Create Note
+        </Button>
+      </Flex>
+      <View
+        name="image"
+        as="input"
+        type="file"
+        style={{ alignSelf: "end" }}
+      />
+    </View>
+    <Heading level={2}>Current Notes</Heading>
+    <View margin="3rem 0">
+      {notes.map((note) => (
+        <Flex
+          key={note.id || note.name}
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Text as="strong" fontWeight={700}>
+            {note.name}
+          </Text>
+          <Text as="span">{note.description}</Text>
+          {note.image && (
+            <Image
+              src={note.image}
+              alt={`visual aid for ${notes.name}`}
+              style={{ width: 400 }}
+            />
+          )}
+          <Button variation="link" onClick={() => deleteNote(note)}>
+            Delete note
           </Button>
         </Flex>
-        <View
-          name="image"
-          as="input"
-          type="file"
-          style={{ alignSelf: "end" }}
-        />
-      </View>
-      <Heading level={2}>Current Notes</Heading>
-      <View margin="3rem 0">
-        {notes.map((note) => (
-          <Flex
-            key={note.id || note.name}
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Text as="strong" fontWeight={700}>
-              {note.name}
-            </Text>
-            <Text as="span">{note.description}</Text>
-            {note.image && (
-              <Image
-                src={note.image}
-                alt={`visual aid for ${notes.name}`}
-                style={{ width: 400 }}
-              />
-            )}
-            <Button variation="link" onClick={() => deleteNote(note)}>
-              Delete note
-            </Button>
-          </Flex>
-        ))}
-      </View>
-      <Button onClick={signOut}>Sign Out</Button>
+      ))}
     </View>
-  );
-};
-
+    <Button onClick={signOut}>Sign Out</Button>
+  </View>
+);
 export default withAuthenticator(App);
